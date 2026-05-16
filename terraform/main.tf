4dec5ec87d7f33b94b05421d6f00551bd38a7eb0
EC2 Default User <ec2-user@ip-172-31-26-159.ec2.internal>
2026-05-16 08:10:14 +0000

    correcting s3

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,5 +3,32 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "example" {
-  bucket = "my-insecure-devsecops-bucket"
+  bucket = "my-secure-devsecops-bucket"
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.example.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
+  bucket = aws_s3_bucket.example.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "block_public" {
+  bucket = aws_s3_bucket.example.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
